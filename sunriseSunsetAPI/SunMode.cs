2a4fd66bc8721e8c56ecc040ac801a48a083e7b7
entertthk <markkc2@hotmail.com>
2020-01-22 12:47:40 +0200

    sunrise

---
+++ sunriseSunsetAPI/SunMode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sunriseSunsetAPI
+{
+    class SunMode
+    {
+        public string Status { get; set; }
+        public Results Results { get; set; }
+
+
+    }
+
+    public class Results
+    {
+        public string Sunrise { get; set; }
+        public string Sunset { get; set; }
+    }
+}
